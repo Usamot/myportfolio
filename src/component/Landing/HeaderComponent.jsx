aa2c5af9a6fd06c13cfa924c4685ad3edc2fa2af
Usamot <57453355+Usamot@users.noreply.github.com>
2026-03-12 16:38:25 +0100

    Updated layout

--- src/component/Landing/HeaderComponent.jsx
+++ src/component/Landing/HeaderComponent.jsx
@@ -1,38 +1,27 @@
-import React from 'react'
-import './header-component.css'
-import con2 from '../assets/con2.png'
+import React from 'react';
+import './header-component.css';
+import con2 from '../assets/con2.png';
 
 const HeaderComponent = () => {
-
-
-
-
   return (
-    <div className="header-component" >
-        
-
-        <img src={con2} alt="" srcset="" />
-
-        <div className="header-component-top">
-
-            <div className='card'>
-
-            <h1 className="top-1">Hi Welcome Here </h1>
-            <h3>I am a Fullstack Developer</h3>
-
-            </div>
-
-            
-            <button type="submit">EMPLOY ME</button>
+    <div className="header-component">
+      <div className="hero-container">
+        {/* Left: Image */}
+        <div className="hero-image">
+          <img src={con2} alt="Hero" />
         </div>
 
-        
-
-
-        
-    
+        {/* Right: Text + Card + Button */}
+        <div className="hero-content">
+          <div className="card">
+            <h1 className="top-1">Hi, Welcome Here</h1>
+            <h3>I am a Fullstack Developer</h3>
+          </div>
+          <button>EMPLOY ME</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
